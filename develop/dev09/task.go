0596658a1865fd353acc81cb81e2290eb7ce3203
beus <burcev-egor@mail.ru>
2023-01-14 21:19:11 +0500

    task 9 changes

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -13,9 +13,17 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// Путь к директории для загрузки файлов
 var workingDir string
+
+// Список загруженных файлов
 var treeMap map[string]bool
+
+// Протокол, по которому загружаются все файлы
 var scheme string
+
+// Максимальная глубина рекурсии. По умолчанию -1 для скачивания всего сайта.
+// Скачиванием сайта назовем загрузку всех файлов, ссылки на которые содержатся в исходном файле(html-документе) и содержащие исходный домен.
 var maxDepth int
 
 func main() {
@@ -26,9 +34,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err != nil {
-		log.Println(err)
-	}
 	os.Mkdir(parsedURL.Host, 0777)
 	os.Chdir(parsedURL.Host)
 	workingDir, _ = os.Getwd()
@@ -41,11 +46,13 @@ func main() {
 	}
 	treeMap = make(map[string]bool)
 	scheme = parsedURL.Scheme
-	maxDepth = 1
+	maxDepth = -1
 	downloadFile(parsedURL.Path, parsedURL.Host, parsedURL.Path, 0)
 }
 
+// Функция для рекурсивной загрузки файлов
 func downloadFile(fileName string, host, path string, depth int) (*os.File, error) {
+	log.Println(fileName, host, path, depth)
 	var recursive = true
 	if depth == maxDepth {
 		recursive = false
@@ -76,9 +83,13 @@ func downloadFile(fileName string, host, path string, depth int) (*os.File, erro
 
 	// Create the file
 	out := createFile(fileName)
-	io.Copy(out, resp.Body)
+	log.Printf("copying %s ...\n", out.Name())
+	_, err = io.Copy(out, resp.Body)
+
 	out.Close()
+
 	out, err = os.Open(out.Name())
+
 	if err != nil {
 		log.Println(err)
 		return nil, err
@@ -87,11 +98,13 @@ func downloadFile(fileName string, host, path string, depth int) (*os.File, erro
 	if err != nil {
 		log.Println(err)
 	}
+
 	document, err := goquery.NewDocumentFromReader(out)
 	if err != nil {
 		log.Println("Error loading HTTP response body. ", err)
 		return nil, err
 	}
+	downloadStatic(host, document)
 	if !recursive {
 		return out, nil
 	}
@@ -126,6 +139,7 @@ func downloadFile(fileName string, host, path string, depth int) (*os.File, erro
 	return out, nil
 }
 
+// Создание дерева из директорий и открытие файла
 func createFile(fileName string) (file *os.File) {
 	path := strings.Split(fileName, "/")
 	var err error
@@ -135,7 +149,7 @@ func createFile(fileName string) (file *os.File) {
 		os.Chdir(path[i])
 	}
 	if i == 0 {
-		file, err = os.Create("index.html")
+		file, err = os.Create(fileName)
 	} else {
 		file, err = os.Create(path[i])
 	}
@@ -143,7 +157,46 @@ func createFile(fileName string) (file *os.File) {
 	if err != nil {
 		log.Printf("could not create file: %s", err)
 		file, err = os.Create("index.html")
+		if err != nil {
+			log.Println("links duplicate")
+			os.Exit(-1)
+		}
 	}
 	log.Printf("created file: %s\n", file.Name())
 	return
+}
+
+// Загрузка статических файлов
+func downloadStatic(host string, document *goquery.Document) {
+	document.Find("link").Each(func(i int, element *goquery.Selection) {
+		href, exists := element.Attr("href")
+		log.Println("static:    ", href, exists)
+		if exists {
+			parsedURL, err := url.Parse(href)
+			if err == nil {
+				if parsedURL.Host == "" {
+					parsedURL.Host = host
+				}
+				downloadFile(parsedURL.Path, parsedURL.Host, parsedURL.Path, maxDepth)
+			} else {
+				log.Println(err)
+			}
+		}
+	})
+	document.Find("img").Each(func(i int, element *goquery.Selection) {
+		href, exists := element.Attr("src")
+		log.Println("static:    ", href, exists)
+		if exists {
+			parsedURL, err := url.Parse(href)
+			if err == nil {
+				if parsedURL.Host == "" {
+					parsedURL.Host = host
+				}
+				downloadFile(parsedURL.Path, parsedURL.Host, parsedURL.Path, maxDepth)
+			} else {
+				log.Println(err)
+			}
+		}
+	})
+
 }
